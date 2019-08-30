e3d405eef42cde68187c36d1bca77c216a783c2e
Salatiel Osorno <salatiel.linares@gmail.com>
2019-08-30 16:39:33 -0500

    Se agrega los archivos necesarios para creacion de test unitarios usando Mocha. Se agregan archivos para uso de variables de ambiente. Se agrega primer prueba y el codigo necesario para cubrirla.

--- test/test.js
+++ test/test.js
@@ -1,19 +1,39 @@
 var chai = require('chai');
 var chaiHttp= require('chai-http');
 
+require('dotenv').config()
+var explorationsDAO = require('../dao/explorationsDAO');
+const MongoClient = require('mongodb').MongoClient;
+
 chai.use(chaiHttp);
 
 var should = chai.should();
 
 var app = require('../app');
 
-describe('Test',() => {
-    it('User test', (done) => {
+describe('Test URL\'s endpoint',() => {
+    before((done) => {
+        MongoClient.connect(process.env.EVA_SERVER, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(async client => {
+            await explorationsDAO.setDB(client);
+            done();
+        })
+        .catch((error) => {
+            console.error(`Ocurrio un error durante la conexion: ${error}`)
+        })
+    })
+    it('Get all explorations', (done) => {
         chai.request(app)
-        .get('/users')
+        .get('/explorations')
         .end((err, res) => {
-            res.should.have.status(201)
-            done(err);
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.have.a.property('explorations');
+            res.body.explorations.should.be.a('array');
+            res.body.should.have.a.property('total');
+            res.body.total.should.be.a('number');
+            res.body.total.should.equals(50000);
+            done();
         })
     })
 })
